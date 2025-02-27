0bfcf0e76e69b45b9c47604e464e7484af9459be
MAMIDIPALLY MANISH <mamidipallymanish6@gmail.com>
2025-02-28 00:04:10 +0530

    images for the Amazon clone

---
+++ assests/first.js
@@ -0,0 +1,8 @@
+let str="Manish Mamidipally   ";
+console.log(str.length);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str.trim());
+console.log(str.slice(0,10));
+str=str.replace("king ")
+console.log(str);
